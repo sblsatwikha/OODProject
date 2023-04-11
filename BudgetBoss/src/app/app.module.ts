6558c4751f2eec8f9b5a367d78ab26cb9d52ab7d
Satwikha <sblsatwikha@gmail.com>
2023-04-10 22:49:04 -0400

    update subscription,expense

--- BudgetBoss/src/app/app.module.ts
+++ BudgetBoss/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy, NavParams } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule ,ReactiveFormsModule} from '@angular/forms';
@@ -11,11 +11,12 @@ import { AuthService } from './services/AuthService.service';
 import { IonicStorageModule } from '@ionic/storage-angular';
 import { CookieService } from 'ngx-cookie-service';
 import { HttpClientModule } from '@angular/common/http';
+import { Tab3Page } from './tab3/tab3.page';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(),IonicModule, AppRoutingModule,FormsModule,ReactiveFormsModule,NgChartsModule,IonicStorageModule.forRoot(), HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy, },AuthService,CookieService],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy, },AuthService,CookieService, Tab3Page, NavParams],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
